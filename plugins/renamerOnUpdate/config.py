243cbf3d7b97cb6f9f97c6da13215f7d2a0bea3a
IAmKontrast <>
2021-11-17 18:56:25 +0100

    Refactor comments and texts in config and README

--- plugins/renamerOnUpdate/config.py
+++ plugins/renamerOnUpdate/config.py
@@ -1,40 +1,56 @@
 ###################################################################
-#
+#       General information         #
 # -----------------------------------------------------------------
-# Available: $date $year $performer $title $height $resolution $studio $parent_studio $studio_family $video_codec $audio_codec
-# -note:
-# $studio_family: If parent studio exist use it, else use the studio name.
-# $performer: If more than * performers, this field will be ignored. Limit to fix at Settings section below (default: 3)
+# Available elements for renaming:
+#   $date 
+#   $year 
+#   $performer 
+#   $title 
+#   $height 
+#   $resolution 
+#   $studio 
+#   $parent_studio 
+#   $studio_family 
+#   $video_codec 
+#   $audio_codec
+#
+# Note:
+# $studio_family: If parent studio exists use it, else use the studio name.
+# $performer: If more than * performers linked to the scene, this field will be ignored. Limit this number at Settings section below (default: 3)
 # $resolution: SD/HD/UHD/VERTICAL (for phone) | $height: 720p 1080p 4k 8k
 # -----------------------------------------------------------------
-# e.g.:
+# Example templates:
+# 
 # $title                                    == Her Fantasy Ball
 # $date $title                              == 2016-12-29 Her Fantasy Ball
+# $date.$title                              == 2016-12-29.Her Fantasy Ball
 # $year $title $height                      == 2016 Her Fantasy Ball 1080p
+# $year_$title-$height                      == 2016_Her Fantasy Ball-1080p
 # $date $performer - $title [$studio]       == 2016-12-29 Eva Lovia - Her Fantasy Ball [Sneaky Sex]
 # $parent_studio $date $performer - $title  == Reality Kings 2016-12-29 Eva Lovia - Her Fantasy Ball
 #
 ####################################################################
+
 #               TEMPLATE             #
 
 # Priority : Tags > Studios > Default
 
-# templates to use for given tags
-# add or remove as needed
+# Templates to use for given tags
+# Add or remove as needed or leave it empty/comment out
 tag_templates = {
-    "!1. Western": "$date $performer - $title [$studio]",
-    "!1. JAV": "$title",
-    "!1. Anime": "$title $date [$studio]"
+    # "!1. Western": "$date $performer - $title [$studio]",
+    # "!1. JAV": "$title",
+    # "!1. Anime": "$title $date [$studio]"
 }
 
-# adjust the below if you want to use studio names instead of tags for the renaming templates
+# Adjust the below if you want to use studio names instead of tags for the renaming templates
 studio_templates = {
 
 }
 
-# change to True to use the default template if no specific tag/studio is found
+# Change to True to use the default template if no specific tag/studio is found
 use_default_template = False
-# default template, adjust as needed
+# Default template, adjust as needed
 default_template = "$date $title"
 
 ######################################
@@ -50,9 +66,9 @@ log_file = r""
 
 # Character to use as a performer separator.
 performer_splitchar = " "
-# Maximum number of performer names in the filename. If there are more than that in a scene the filename will not include any performer names!
+# Maximum number of performer names in the filename. If there are more than that in a scene the filename will not include any performer name!
 performer_limit = 3
-# ignore male performers.
+# Ignore male performers.
 performer_ignore_male = False
 
 # If $performer is before $title, prevent having duplicate text. 
@@ -64,7 +80,7 @@ prevent_title_performer = False
 # Only rename 'Organized' scenes.
 only_organized = False
 # Field to remove if the path is too long. First in list will be removed then second then ... if length is still too long.
-order_field = ["$video_codec", "$audio_codec", "$resolution", "$height", "$studio_family", "$studio", "$parent_studio","$performer"]
+order_field = ["$video_codec", "$audio_codec", "$resolution", "$height", "$studio_family", "$studio", "$parent_studio", "$performer"]
 # Alternate way to show diff. Not useful at all.
 alt_diff_display = False
 
