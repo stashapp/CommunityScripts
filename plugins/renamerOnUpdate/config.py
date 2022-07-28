1075bd80bd5131299bcc981e72ff003f0f714b5d
Belleyy <Belleyy@users.noreply.github.com>
2022-07-28 21:00:44 +0200

    Update config.py

--- plugins/renamerOnUpdate/config.py
+++ plugins/renamerOnUpdate/config.py
@@ -15,11 +15,16 @@
 #   $tags
 #   $video_codec 
 #   $audio_codec
+#   $movie_scene
+#   $movie_title
+#   $movie_year
+#   $movie_scene
 #
 # Note:
 # $studio_family: If parent studio exists use it, else use the studio name.
 # $performer: If more than * performers linked to the scene, this field will be ignored. Limit this number at Settings section below (default: 3)
 # $resolution: SD/HD/UHD/VERTICAL (for phone) | $height: 720p 1080p 4k 8k
+# $movie_scene: "scene #" # = index scene
 # -----------------------------------------------------------------
 # Example templates:
 # 
@@ -36,7 +41,8 @@
 #                STASH               #
 stash_url = "localhost"
 
-#               TEMPLATE             #
+####################################################################
+#           TEMPLATE FILENAME (Rename your files)
 
 # Priority : Tags > Studios > Default
 
@@ -58,6 +64,41 @@ use_default_template = False
 # Default template, adjust as needed
 default_template = "$date $title"
 
+####################################################################
+#           TEMPLATE PATH  (Move your files)
+
+# $studio_hierarchy: create the whole hierarchy folder (MindGeek/Brazzers/Hot And Mean/video.mp4)
+# ^* = parent of folder (E:\Movies\video.mp4 -> E:\Movies\)
+
+# trigger with a specific tag
+# "tagname": "path"
+# ex: "plugin_move": r"E:\Movies\R18\$studio_hierarchy"
+p_tag_templates = {
+    "plugin_move_atrier": r"E:\Film\R18\2. Test\$studio_hierarchy"
+}
+
+
+p_studio_templates = {
+}
+
+# match a path
+# "match path": "destination"
+# ex: r"E:\Film\R18\2. Test\A trier": r"E:\Film\R18\2. Test\A trier\$performer",
+p_path_templates = {
+}
+
+# change to True to use the default template if no specific tag/studio is found
+p_use_default_template = False
+# default template, adjust as needed
+p_default_template = r"^*\$performer"
+
+# option if tag is present
+# "tagname": [option]
+# clean_tag: remove the tag after the rename
+# inverse_performer: change the last/first name (Jane Doe -> Doe Jane)
+# ex: "plugin_move": ["clean_tag"]
+p_tag_option = {
+}
 ######################################
 #               Logging              #
 
@@ -91,6 +132,10 @@ removecharac_Filename = ",#"
 performer_splitchar = " "
 # Maximum number of performer names in the filename. If there are more than that in a scene the filename will not include any performer name!
 performer_limit = 3
+# The filename with have the name of performer before reaching the limit (if limit=3, the filename can contains 3 performers for a 4 performers scenes)
+performer_limit_keep = False
+# sorting performer (name, id, rating, favorite, mix (favorite > rating > name), mixid (..>..> id))
+performer_sort = "id"
 # ignore certain gender. Available "MALE" "FEMALE" "TRANSGENDER_MALE" "TRANSGENDER_FEMALE" "INTERSEX" "NON_BINARY"
 performer_ignoreGender = []
 
@@ -99,6 +144,16 @@ performer_ignoreGender = []
 # Template used: $year $performer - $title
 # 2016 Dani Daniels - Dani Daniels in ***.mp4 --> 2016 Dani Daniels in ***.mp4
 prevent_title_performer = False
+
+## Path mover related
+# remove consecutive (/FolderName/FolderName/video.mp4 -> FolderName/video.mp4
+prevent_consecutive = True
+# check when the file has moved that the old directory is empty, if empty it will remove it.
+remove_emptyfolder = True
+# if there is no performer on the scene, the $performer field will be replaced by "NoPerformer" so a folder "NoPerformer" will be created
+path_noperformer_folder = False
+# if the folder already have a performer name, it won't change it
+path_keep_alrperf = True
 
 # Removes prepositions from the beginning of titles
 prepositions_list = ['The', 'A', 'An']
@@ -146,8 +201,13 @@ order_field = ["$video_codec", "$audio_codec", "$resolution", "tags", "rating", 
 # Alternate way to show diff. Not useful at all.
 alt_diff_display = False
 
+# number of scene process by the task renamer. -1 = all scenes
+batch_number_scene = -1
+
 # disable/enable the hook. You can edit this value in 'Plugin Tasks' inside of Stash.
 enable_hook = True
+# disable/enable dry mode. This prevent any change, it will only show in log. You can edit this value in 'Plugin Tasks' inside of Stash.
+dry_run = False
 ######################################
 #            Module Related          #
 
