3d8ce70b53cd7a275aa5862f2237b9d35dfd1e4d
HijackHornet <NorthRiver@laposte.net>
2023-06-17 23:21:13 +0200

    Added folder based gallery support

--- plugins/DateParser/date_parser.py
+++ plugins/DateParser/date_parser.py
@@ -35,14 +35,23 @@ def find_date_for_galleries():
 			"value": 1
 		}
 	})
+	galleriesNoZip = stash.find_galleries(f={
+		"is_missing": "date",
+		"path": {
+			"modifier": "NOT_MATCHES_REGEX",
+			"value": ".zip$"
+		},
+	})
 
+	totalZipGalleries = len(galleries)
+	
 
-	total = len(galleries)
-
-	log.info(f"Found {total} galleries")
+	log.info(f"Found {totalZipGalleries} zip galleries")
+	log.info(f"Found {len(galleriesNoZip)} non zip galleries")
+	total = totalZipGalleries + len(galleriesNoZip)
 
 	for i, gallery in enumerate(galleries):
-		log.progress(i/total)
+		log.progress(i+1/total)
 		acceptableDate = None
 		for file in gallery.get("files", []):
 			for match in pattern.finditer(file["path"]):
@@ -60,7 +69,28 @@ def find_date_for_galleries():
 			}
 			stash.update_gallery(updateObject)
 				
+	
 
+	for i, gallery in enumerate(galleriesNoZip):
+		
+		log.progress(i+totalZipGalleries+1/total)
+		acceptableDate = None
+		path = gallery["path"]
+		for match in pattern.finditer(path):
+			g1 = match.group(1)
+			g2 = match.group(2)
+			g3 =match.group(3)
+			temp = parse(g1+" "+g2+" "+g3)
+			if temp:
+				acceptableDate = temp.strftime("%Y-%m-%d")
+				break
+		if acceptableDate:
+			log.info("Gallery ID ("+gallery.get("id") + ") has matched the date : "+acceptableDate)
+			updateObject = {
+				"id":gallery.get("id"),
+				"date":acceptableDate
+			}
+			stash.update_gallery(updateObject)
 
 if __name__ == '__main__':
 	main()
