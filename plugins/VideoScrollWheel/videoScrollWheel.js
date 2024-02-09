896fda00b9514f2b0db346878753f30a44b1c387
WeedLordVegeta420 <81525421+WeedLordVegeta420@users.noreply.github.com>
2024-02-08 19:53:57 -0500

    Add VideoScrollWheel settings.

--- plugins/VideoScrollWheel/videoScrollWheel.js
+++ plugins/VideoScrollWheel/videoScrollWheel.js
@@ -1,17 +1,33 @@
 (function () {
   const volumeScrollScale = -0.00065;
   const timeScrollScale = 0.01;
-
   const timeScrollFriction = 0.00015;
   const timeScrollAcceleration = 0.55;
-  const maxTimeDelta = 2000;
-  const maxTimeScrollVelocity = 5.0;
 
   let vjsPlayer = null;
   let scrollVelocity = 1;
   let previousTime = Date.now();
+  let pluginSettings = {
+    volumeScrollSpeed: 100.0,
+    timeScrollSpeed: 100.0,
+    timeScrollAcceleration: 100.0,
+    minTimeScrollSpeed: 1.0,
+    maxTimeScrollSpeed: 5.0,
+    timeScrollVelocityDecay: 100.0,
+    timeScrollVelocityTimeout: 2000,
+  };
 
-  function setupVideoScrollWheel() {
+  async function setupVideoScrollWheel() {
+    // Get settings
+    var settings = await getPluginConfig("videoScrollWheel");
+    if (settings) {
+      for (var key in settings) {
+        if (pluginSettings.hasOwnProperty(key)) {
+          pluginSettings[key] = settings[key];
+        }
+      }
+    }
+
     // Get video player and register wheel event listener.
     vjsPlayer = document.getElementById("VideoJsPlayer").player;
     var vjsEl = vjsPlayer.el_;
@@ -27,34 +43,69 @@
     var scrollDelta = e.deltaY;
 
     if (targetWidth / 2 > targetX) {
-      // Scrolled on left, change volume.
-      var newVolume = vjsPlayer.volume() + scrollDelta * volumeScrollScale;
+      // Scrolled on left side, change volume.
+      var newVolume =
+        vjsPlayer.volume() +
+        scrollDelta *
+          volumeScrollScale *
+          (pluginSettings.volumeScrollSpeed / 100.0);
       vjsPlayer.volume(newVolume);
     } else {
-      // Scrolled on right, change time.
+      // Scrolled on right side, change time.
       var now = Date.now();
       var deltaTime = now - previousTime;
       if (deltaTime === 0) {
         return;
-      } else if (deltaTime > maxTimeDelta) {
-        scrollVelocity = 1;
+      } else if (
+        deltaTime > pluginSettings.timeScrollVelocityTimeout ||
+        pluginSettings.timeScrollAcceleration === 0
+      ) {
+        scrollVelocity = pluginSettings.minTimeScrollSpeed;
       } else {
-        scrollVelocity =
-          scrollVelocity -
-          scrollVelocity * timeScrollFriction * deltaTime +
-          (1 / deltaTime) * timeScrollAcceleration;
+        var friction =
+          scrollVelocity *
+          timeScrollFriction *
+          deltaTime *
+          (pluginSettings.timeScrollVelocityDecay / 100.0);
+        var acceleration =
+          (1 / deltaTime) *
+          timeScrollAcceleration *
+          (pluginSettings.timeScrollAcceleration / 100.0);
+        scrollVelocity = scrollVelocity - friction + acceleration;
         scrollVelocity = Math.max(
-          1,
-          Math.min(scrollVelocity, maxTimeScrollVelocity),
+          pluginSettings.minTimeScrollSpeed,
+          Math.min(scrollVelocity, pluginSettings.maxTimeScrollSpeed),
         );
       }
 
       previousTime = now;
 
-      var timeDelta = scrollDelta * timeScrollScale * scrollVelocity;
+      var timeDelta =
+        scrollDelta *
+        timeScrollScale *
+        scrollVelocity *
+        (pluginSettings.timeScrollSpeed / 100.0);
       var newTime = vjsPlayer.currentTime() + timeDelta;
       vjsPlayer.currentTime(newTime);
     }
+  }
+
+  // Util functions for getting plugin settings.
+  async function getPluginConfigs() {
+    const reqData = {
+      operationName: "Configuration",
+      variables: {},
+      query: `query Configuration {
+                configuration {
+                  plugins
+                }
+              }`,
+    };
+    return stash.callGQL(reqData);
+  }
+  async function getPluginConfig(pluginId) {
+    const data = await getPluginConfigs();
+    return data.data.configuration.plugins[pluginId];
   }
 
   // Wait for video player to load on scene page.
