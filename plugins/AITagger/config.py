279b8efe16dd7fbb8e4181c1b3ee1a15db0eb43b
skier233 <benchy222@gmail.com>
2024-05-30 08:28:57 -0400

    initial plugin commit

---
+++ plugins/AITagger/config.py
@@ -0,0 +1,9 @@
+API_BASE_URL = 'http://localhost:8000'
+IMAGE_REQUEST_BATCH_SIZE = 320
+CONCURRENT_TASK_LIMIT = 10
+
+ai_base_tag_name = "AI"
+tagme_tag_name = "AI_TagMe"
+updateme_tag_name = "AI_UpdateMe"
+aitagged_tag_name = "AI_Tagged"
+aierrored_tag_name = "AI_Errored"
