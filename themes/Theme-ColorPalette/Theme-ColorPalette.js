b84fc5e0da3302c8ade90172585506318784ed4c
wql219 <160428035+wql219@users.noreply.github.com>
2024-09-28 12:34:39 +0800

    format the code

--- themes/Theme-ColorPalette/Theme-ColorPalette.js
+++ themes/Theme-ColorPalette/Theme-ColorPalette.js
@@ -1,28 +1,34 @@
-if (!localStorage.getItem('UserColor')) {
-  localStorage.setItem('UserColor', 0);
+if (!localStorage.getItem("UserColor")) {
+  localStorage.setItem("UserColor", 0);
 }
-document.documentElement.style.setProperty('--maincolorDeg', localStorage.getItem('UserColor') +"deg");
+document.documentElement.style.setProperty(
+  "--maincolorDeg",
+  localStorage.getItem("UserColor") + "deg"
+);
 
 async function mainFunction() {
   let hueValue;
   try {
-      const config = await csLib.getConfiguration("colorPalette", {});
-      hueValue = config.hue;
-      let UserColorValue = hueValue;
-      localStorage.setItem('UserColor', UserColorValue);
+    const config = await csLib.getConfiguration("colorPalette", {});
+    hueValue = config.hue;
+    let UserColorValue = hueValue;
+    localStorage.setItem("UserColor", UserColorValue);
   } catch (error) {
-      console.error('Error getting configuration:', error);
+    console.error("Error getting configuration:", error);
   }
   return hueValue;
 }
 mainFunction();
 
-
-if (!localStorage.getItem('UserColor')) {
-  localStorage.setItem('UserColor', 0);
+if (!localStorage.getItem("UserColor")) {
+  localStorage.setItem("UserColor", 0);
 }
-document.documentElement.style.setProperty('--maincolorDeg', localStorage.getItem('UserColor') +"deg");
+document.documentElement.style.setProperty(
+  "--maincolorDeg",
+  localStorage.getItem("UserColor") + "deg"
+);
 
-document.querySelector("meta[name='theme-color']").content = window.getComputedStyle(document.querySelectorAll(".top-nav")[0]).backgroundColor
-
-
+document.querySelector("meta[name='theme-color']").content =
+  window.getComputedStyle(
+    document.querySelectorAll(".top-nav")[0]
+  ).backgroundColor;
