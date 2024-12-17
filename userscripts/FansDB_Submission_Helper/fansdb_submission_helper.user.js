94dc4d2e075d646c652722ff82178428ab848cb5
DogmaDragon <103123951+DogmaDragon@users.noreply.github.com>
2024-12-17 05:14:27 +0200

    Fix typo

--- userscripts/FansDB_Submission_Helper/fansdb_submission_helper.user.js
+++ userscripts/FansDB_Submission_Helper/fansdb_submission_helper.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        FansDB Submission Helper
 // @author      mmenanno, DogmaDragon
-// @version     0.8.5
+// @version     0.8.6
 // @description Adds button to add all unmatched aliases, measurements, and urls to a performer.
 // @icon        https://raw.githubusercontent.com/FansDB/docs/main/docs/assets/images/favicon.png
 // @namespace   FansDB-Submission-Helper
@@ -455,7 +455,7 @@ const urlPatterns = [
     site: "MemberMe",
   },
   {
-    pattern: /(^https?:\/\/(?:www.)?share)\.myfreecams\.com\/[^?]+)/,
+    pattern: /(^https?:\/\/(?:www.)?share\.myfreecams\.com\/[^?]+)/,
     site: "MFC Share",
   },
   {
