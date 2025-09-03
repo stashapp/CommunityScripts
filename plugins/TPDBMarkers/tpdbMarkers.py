377ba6d3036b5e7ececa17e7150aee3faead1898
donlothario <rossgelle67@gmail.com>
2025-09-03 04:23:49 +0200

    Fix GraphQL Validation is Failing for timestampTrade and TPBMarkers (#608)

--- plugins/TPDBMarkers/tpdbMarkers.py
+++ plugins/TPDBMarkers/tpdbMarkers.py
@@ -202,7 +202,9 @@ if TPDB_ENDPOINT in [
         PLUGIN_ARGS = json_input["args"]["mode"]
         if "processScene" == PLUGIN_ARGS:
             if "scene_id" in json_input["args"]:
-                scene = stash.find_scene(json_input["args"]["scene_id"])
+                scene = stash.find_scene(
+                    json_input["args"]["scene_id"],
+                    fragment='id urls stash_ids {endpoint stash_id} groups {scene_index group {id}} tags {id} scene_markers {id} interactive files { path duration fingerprint(type: "phash")}')
                 processScene(scene)
             else:
                 processAll()
